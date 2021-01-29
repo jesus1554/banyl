cf93b2fdd21ecd653abee4af3152b5b2d08f9e08
Jesús Flores <dr.techcontacto@gmail.com>
2021-01-28 18:15:18 -0600

    all tools are inside

--- banyl.py
+++ banyl.py
@@ -1,1 +1,18 @@
-import this
+# Banyl 2021
+# Copyright (C) jesus1554 MIT Licence
+
+import eyed3
+import requests
+import os
+
+# raw = requests.get('https://api.deezer.com/album/302127')
+
+# res = raw.json()
+# print(res)
+
+
+for file_name in os.listdir('exampledir/'):
+    print(file_name)
+
+audiofile = eyed3.load("exampledir/Billie Eilish - bad guy.mp3")
+print(audiofile.tag.album)
